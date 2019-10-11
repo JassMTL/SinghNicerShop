1fe8ca1850c5f6e447728437a60505227f26a8b3
Jass <jaspreet.jas@hotmail.com>
2019-10-11 11:08:45 -0400

    finished with part 3.

--- app/src/main/java/com/example/singhnicershop/MenuActivity.java
+++ app/src/main/java/com/example/singhnicershop/MenuActivity.java
@@ -7,6 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.example.singhnicershop.model.ShoppingItems;
+
 public class MenuActivity extends AppCompatActivity {
 
     @Override
@@ -24,6 +26,37 @@ public class MenuActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
+        String [] title = {
+                getResources().getString(R.string.sarbloh_karra),
+                getResources().getString(R.string.kirpan),
+                getResources().getString(R.string.pendant)
+        };
+
+        String [] description = {
+                getResources().getString(R.string.descKarra),
+                getResources().getString(R.string.descriptionKirpan),
+                getResources().getString(R.string.descriptionPendant)
+        };
+
+        String [] price = {
+                getResources().getString(R.string.priceKarraStr),
+                getResources().getString(R.string.priceKirpanStr),
+                getResources().getString(R.string.pricePendant)
+        };
+
+        int [] image = {
+                R.drawable.karra,
+                R.drawable.kirpan,
+                R.drawable.khanda
+        };
+
+        String quantity = getResources().getString(R.string.defaultNum);
+
+        for(int i = 0; i < title.length;i++){
+            new ShoppingItems(title[i],description[i],price[i],image[i],quantity);
+        }
     }
 
 }
--- app/src/main/java/com/example/singhnicershop/model/ShoppingItems.java
+++ app/src/main/java/com/example/singhnicershop/model/ShoppingItems.java
@@ -1,4 +1,57 @@
 package com.example.singhnicershop.model;
 
 public class ShoppingItems {
+    String title;
+    String description;
+    String price;
+    int image;
+    String quantity;
+
+    public ShoppingItems(String title,String description,String price,int image,String quantity){
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.image = image;
+        this.quantity = quantity;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
 }
